5a2f4e96eca0378abb4098ba74b6c71229e15e3c
tropikss <Mathis>
2024-02-01 08:24:48 +0100

    hgh

--- main.py
+++ main.py
@@ -4,17 +4,21 @@ import mido
 port = mido.get_input_names()
 print(mido.get_input_names())
 
+notes_appuyees = set()
+
 # Choisis le port MIDI approprié
 port_name = port[1]
 midi_port = mido.open_input(port_name)
 
-print("Lecture des messages MIDI. Appuie sur Ctrl+C pour arrêter.")
 oldmsg = mido.Message('note_on', note=0, velocity=0, time=0)
 try:
     for msg in midi_port:
-        if(msg != oldmsg):
-            print(msg)
-            oldmsg = msg
+        if msg.type == 'note_on' and msg.velocity > 0:
+            notes_appuyees.add(msg.note)
+            print(f"Notes appuyées : {notes_appuyees}")
+        elif msg.type == 'note_off' or (msg.type == 'note_on' and msg.velocity == 0):
+            notes_appuyees.discard(msg.note)
+            print(f"Notes appuyées : {notes_appuyees}")
 except KeyboardInterrupt:
     print("\nLecture arrêtée.")
 finally:
