3e2f88ebca2f2f400f486e2033c18581da2769f0
tropikss <Mathis>
2024-02-06 16:50:36 +0100

    hggh

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import mido
 import neopixel
 
-# Configuration des LEDs
-NUM_LEDS = 75  # Nombre total de LEDs dans la bande
-PIN_LED = 18  # Port GPIO auquel les LEDs sont connectées
-BRIGHTNESS = 0.5  # Luminosité des LEDs (0 à 1)
+import time
+from rpi_ws281x import *
+import argparse
 
-# Initialisation de la bande de LEDs
-pixels = neopixel.NeoPixel(PIN_LED, NUM_LEDS, brightness=BRIGHTNESS, auto_write=False)
+# LED strip configuration:
+LED_COUNT      = 75     # Number of LED pixels.
+LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
+#LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA        = 10      # DMA channel to use for generating a signal (try 10)
+LED_BRIGHTNESS = 65      # Set to 0 for darkest and 255 for brightest
+LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
 # Allumer 3 LEDs en rouge
 for i in range(3):
