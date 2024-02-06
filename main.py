fad7a1f5cd10c32d4f3d055cc4224d3ee79d09c4
tropikss <Mathis>
2024-02-06 17:41:25 +0100

    ghgh

--- main.py
+++ main.py
@@ -43,11 +43,11 @@ try:
         if msg.type == 'note_on' and msg.velocity > 0:
             notes_appuyees.add(msg.note)
             v = int(((msg.note-22)/88)*75)
-            led(v)
+            ledOn(v)
             print(f"Notes appuyées : {notes_appuyees}")
         elif msg.type == 'note_off' or (msg.type == 'note_on' and msg.velocity == 0):
             v = int(((msg.note-22)/88)*75)
-            led(v)
+            ledOff(v)
             notes_appuyees.discard(msg.note)
             print(f"Notes appuyées : {notes_appuyees}")
 except KeyboardInterrupt:
