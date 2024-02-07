3cc698c3eac75810b5b0cb8f2fb4a1b6f2087c2a
Mathis <mathis.friess@gmail.com>
2024-02-07 12:55:55 +0100

    Changements effectués automatiquement

--- main.py
+++ main.py
@@ -90,6 +90,7 @@ oldmsg = mido.Message('note_on', note=0, velocity=0, time=0)
 try:
     for msg in midi_port:
         if(msg.type != 'clock'):
+            print(msg.type)
             nb = msg.note - 21
             temp = ((conversion(nb%12) + (nb//12)*7) / 52) * LED_COUNT - 0.21
             floor = math.floor(temp)
