34f0052f772012a7e402fa2c295d141c14689705
Mathis <mathis.friess@gmail.com>
2024-02-07 19:30:05 +0100

    Changements effectués automatiquement

--- main.py
+++ main.py
@@ -52,7 +52,6 @@ def refresh_strip(couleur):
     to_remove = []
     for i in led_tab:
         if(led_tab[i] > 0):
-            print("ledColor("+str(i)+", "+str(led_tab[i])+")")
             ledColor(i, couleur, led_tab[i])
             led_tab[i] -= STEP
             if(led_tab[i] <= 0):
