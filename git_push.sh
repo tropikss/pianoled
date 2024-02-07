69941625d9c4666d84566aad41867e9bd07adab3
Mathis <mathis.friess@gmail.com>
2024-02-07 12:36:22 +0100

    Changements effectués automatiquement

---
+++ git_push.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Ajoute tous les fichiers modifiés au staging area
+git add .
+
+# Effectue un commit avec un message spécifié
+git commit -m "Changements effectués automatiquement"
+
+# Pousse les changements vers le dépôt distant
+git push
