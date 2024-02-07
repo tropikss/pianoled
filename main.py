a1b713c27ac8bc155d21de8be571eae1e8ad1656
Mathis <mathis.friess@gmail.com>
2024-02-07 11:12:50 +0100

    ghgh

--- main.py
+++ main.py
@@ -83,13 +83,17 @@ try:
         if msg.type == 'note_on' and msg.velocity > 0:
             notes_appuyees.add(msg.note)
 
+            nb = msg.note - 22
             v = math.ceil(((conversion((nb-22)%12) + ((nb-22)//12)*7) / 52) * LED_COUNT)
 
             ledIntensite(v)
             print(f"Notes appuyees : {notes_appuyees}")
 
         elif msg.type == 'note_off' or (msg.type == 'note_on' and msg.velocity == 0):
-            v = math.ceil(((conversion((nb-22)%12) + ((nb-22)//12)*7) / 52) * LED_COUNT)
+
+            nb = msg.note - 22
+            v = math.ceil(((conversion(nb%12) + (nb//12)*7) / 52) * LED_COUNT)
+
             ledOff(v)
             notes_appuyees.discard(msg.note)
             print(f"Notes appuyées : {notes_appuyees}")
