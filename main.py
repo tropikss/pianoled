db4abd574267173262579a76c4bfecc1ceffa9cb
Mathis <mathis.friess@gmail.com>
2024-02-08 10:14:40 +0100

    Changements effectués automatiquement

--- main.py
+++ main.py
@@ -109,7 +109,7 @@ def refresh_strip(velocity):
     to_remove = []
     for i in led_tab:
         if(led_tab[i] > 0):
-            couleur = getColor((i/LED_COUNT)*100)
+            couleur = getColor((velocity/120)*100)
             ledColor(i, couleur, led_tab[i])
             led_tab[i] = round(led_tab[i] - STEP, 2)
         else:
