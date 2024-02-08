812e58065b271064a173aca53216f9d0fa33d2d1
Mathis <mathis.friess@gmail.com>
2024-02-08 10:18:37 +0100

    Changements effectués automatiquement

--- main.py
+++ main.py
@@ -190,10 +190,10 @@ midi_port = mido.open_input(port_name)
 oldmsg = mido.Message('note_on', note=0, velocity=0, time=0)
 
 i = 0
+velocity = 0
 
 try:
     for msg in midi_port:
-        velocity = 0
         if(msg.type != 'clock' and msg.type != 'control_change'):
             nb = msg.note - 21
             temp = ((conversion(nb%12) + (nb//12)*7) / 52) * LED_COUNT - 0.21
